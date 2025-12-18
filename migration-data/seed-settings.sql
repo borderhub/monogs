d66050293f99f8bd0a1580832554ed8ae9559d54
“borderhub” <shirabegroup@gmail.com>
2025-12-18 11:34:47 +0900

    git対象外追加&css調整

--- migration-data/seed-settings.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO settings (id, key, value) VALUES
-  ('setting_1', 'cover_image', ''),
-  ('setting_2', 'description', 'monogs works and art project'),
-  ('setting_3', 'icon', ''),
-  ('setting_4', 'logo', '/content/images/2019/01/----------2019-01-12-20.16.01.png'),
-  ('setting_5', 'navigation', '[{"label":"Home","url":"https://monogs.net"},{"label":"Biography","url":"/biography/"},{"label":"Exhibition","url":"/tag/exhibition/"},{"label":"Works","url":"/tag/works/"},{"label":"Music","url":"/tag/music/"},{"label":"tips","url":"/tag/tips/"},{"label":"diary","url":"/tag/diary/"},{"label":"Links","url":"/links/"}]'),
-  ('setting_6', 'title', 'monogs web site');
